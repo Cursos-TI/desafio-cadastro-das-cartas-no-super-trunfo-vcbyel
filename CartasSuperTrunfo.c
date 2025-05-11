92a2c626c707dc6b66ccf0b79995dd01e15eb974
vcbyel <byel7382@gmail.com>
2025-05-11 13:34:49 -0300

    alguns comentários primeira carta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,6 +1,9 @@
-#include <stdio.h>
+#include <stdio.h> //inclusao da biblioteca
+
 
 int main() {
+
+    // designação de variaveis
     
 char estado[100];
 char codigo[100];
@@ -12,7 +15,7 @@ int numero;
 
 //primeira parte da estruturação da 1º carta
 
-printf ("\n---Dados da Primeira Carta---\n");
+printf ("\n---Insira os Dados da Primeira Carta---\n"); //isso aqui é só cabeçalho, se não precisar eu tiro
 
 printf ("Digite seu Estado:");
     scanf("%s", &estado);
@@ -36,16 +39,19 @@ printf ("Digite seu Estado:");
     scanf("%d", &numero);
     
      // Impressão
-    printf("\n--- Dados da Carta ---\n");
+    
+    printf("\n--- Dados da Primeira Carta ---\n"); // cabeçalho impresso depois de os dados inseridos
     printf("Estado: %s\n", estado);
     printf("Código: %s\n", codigo);
     printf("Cidade: %s\n", cidade);
     printf("População: %d\n", populacao);
-    printf("Área: %.2f km²\n", area);
+    printf("Área: %.2f km²\n", area);   // na minha ide não saiu esse %.2f veio quando trouxe pra cá
     printf("PIB: %.2f\n", pib);
     printf("Nº de Pontos Turísticos: %d\n", numero);
 
-    // ainda nao resolvi a questão de colocar ponto
+    // ainda nao resolvi a questão de colocar ponto na variavel int populacao
+
+    
     
     
     
