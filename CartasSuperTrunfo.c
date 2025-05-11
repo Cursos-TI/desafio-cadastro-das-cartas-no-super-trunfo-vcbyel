8534e56dbc8513d6655287e9d870cb98abc3fbfa
vcbyel <byel7382@gmail.com>
2025-05-11 13:50:00 -0300

    cartas super trunfo pronto

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -45,14 +45,46 @@ printf ("Digite seu Estado:");
     printf("Código: %s\n", codigo);
     printf("Cidade: %s\n", cidade);
     printf("População: %d\n", populacao);
-    printf("Área: %.2f km²\n", area);   // na minha ide não saiu esse %.2f veio quando trouxe pra cá
+    printf("Área: %.3f km²\n", area);   // na minha ide não saiu esse %.2f veio quando trouxe pra cá
     printf("PIB: %.2f\n", pib);
     printf("Nº de Pontos Turísticos: %d\n", numero);
 
     // ainda nao resolvi a questão de colocar ponto na variavel int populacao
 
     
+    printf ("\n---Insira Dados da Segunda Carta---\n");
+
+printf ("Digite seu Estado:");
+    scanf("%s", &estado2);
+
+    printf ("Código da Carta:");
+    scanf("%s", codigo2);
+
+    printf ("Cidade:");
+    scanf("%s", cidade2);
+
+    printf ("População:");
+    scanf("%d", &populacao2);
+
+    printf ("Área Total: ");
+    scanf("%f", &area2);
     
+    printf ("PIB:");
+    scanf("%f", &pib2);
+    
+    printf ("Número de pontos Turísticos: ");
+    scanf("%d", &numero2);
+    
+     // Impressão
+    
+    printf("\n--- Dados da Segunda Carta ---\n");
+    printf("Estado: %s\n", estado2);
+    printf("Código: %s\n", codigo2);
+    printf("Cidade: %s\n", cidade2);
+    printf("População: %d\n", populacao2);
+    printf("Área: %.3f km²\n", area2);
+    printf("PIB: %.2f\n", pib2);
+    printf("Nº de Pontos Turísticos: %d\n", numero2);
     
     
     // Cadastro das Cartas:
